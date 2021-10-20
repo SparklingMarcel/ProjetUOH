50400ba11297cc4058c67bef6666423721cb9264
nhonorisg <Honoris-gratias.NDEREYIMANA@crie.i-univ-tlse2.fr>
2021-10-20 10:57:27 +0200

    suppression de l'import 'java.net.SocketException' car on ne l'utilise plus.

--- src/InspectWebLinks.java
+++ src/InspectWebLinks.java
@@ -3,7 +3,6 @@ package src;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.SocketException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
