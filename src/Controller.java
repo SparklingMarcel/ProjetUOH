a450d897714b2ac08cfb99a5a41b20d516a067a4
JulienGendre <43234586+JulienGendre@users.noreply.github.com>
2021-10-20 15:46:54 +0200

    les classes et le fxml de l'interface

---
+++ src/Controller.java
@@ -0,0 +1,9 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    private void lancer(ActionEvent event) {
+        InspectWebLinks.main();
+    }
+}
---
+++ src/UOHinterface.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class UOHinterface extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        System.out.println((getClass().getResource("sample.fxml")));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("UOH Liens Morts");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+}
